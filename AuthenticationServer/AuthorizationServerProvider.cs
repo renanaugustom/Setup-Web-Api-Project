9ae86d5de19ea440c67ab8ebbf60797b47f24815
Renan Augusto <renan.augusto18@hotmail.com>
2015-08-08 15:43:16 -0300

    Authentication Server - Backend

---
+++ AuthenticationServer/AuthorizationServerProvider.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AuthenticationServer
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        /// <summary>
+        /// Método responsável por validar o Token em cache. (Responsabilidade do OAuth).
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            try
+            {
+                var user = context.UserName;
+                var password = context.Password;
+
+                //Aplique sua lógica para autenticação
+                if(user != "renan" || password != "renan")
+                {
+                    context.SetError("invalid_grant", "Usuário ou senha inválidos");
+                    return;
+                }
+
+                //Adiciona o nome do usuário
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim(ClaimTypes.Name, user));
+
+                //Exemplo de grupo de usuários.
+                var roles = new List<string>();
+                roles.Add("User");
+
+                foreach (var role in roles)
+                {
+                    identity.AddClaim(new Claim(ClaimTypes.Role, role));
+                }
+
+                GenericPrincipal principal = new GenericPrincipal(identity, roles.ToArray());
+                Thread.CurrentPrincipal = principal;
+
+                context.Validated(identity);
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+
+    }
+}
