8e73eadd5439796f8def938482d1698893e0eb7d
Renan Augusto <renan.augusto18@hotmail.com>
2015-08-08 16:22:33 -0300

    WebApplication Structure

---
+++ WebApplication/Scripts/Base/app.js
@@ -0,0 +1,9 @@
+﻿angular.module('app.controllers', []);
+angular.module('app.services', []);
+
+var app = angular.module("MyApp", ['app.controllers', 'app.services', 'ui.router', 'angular-storage']);
+
+app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+    
+
+});
